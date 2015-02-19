ad50e016918e655820ca3f5ad7a8f1e59a5cc9e5
Stephen Richards <stephen@mimi.local>
2015-02-19 11:44:35 +0000

    Add Access-Control-Allow-Origin headers to all responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+  before_filter :add_headers
+
+
+  private
+
+  def add_headers
+	  response.headers["Access-Control-Allow-Origin"] = "http://localhost:9000"
+	end
 end
