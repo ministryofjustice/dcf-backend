b4f7aca1b64563b8ba56f3e54d5f316d2a3bdbd4
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-02-24 16:51:38 +0000

    Allow Access-Control-Allow-Origin headers from multiple origins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+
+	ALLOWED_HOSTS = [
+		'http://localhost:3000',
+		'http://localhost:9000',
+		'https://dcf-frontend.herokuapp.com'
+	]
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
@@ -7,7 +15,29 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def add_headers
-	  response.headers["Access-Control-Allow-Origin"] = "http://localhost:9000"
+  def remote_hostname
+	  require 'resolv'
+	  Resolv.getname(request.remote_ip)
 	end
+
+  def add_headers
+  	ALLOWED_HOSTS.each do |allowed_host|
+  		allowed_host_uri = URI(allowed_host)
+  		request_uri = URI(request.env["REQUEST_URI"])
+  		if uris_match?(allowed_host_uri, request_uri)
+  			response.headers["Access-Control-Allow-Origin"] = host_from_uri(allowed_host_uri)
+  			break
+  		end
+  	end
+	end
+
+
+	def host_from_uri(uri)
+		"#{uri.scheme}://#{uri.host}:#{uri.port}"
+	end
+
+	def uris_match?(a, b)
+		a.scheme == b.scheme && a.host == b.host && a.port == b.port
+	end
+
 end
